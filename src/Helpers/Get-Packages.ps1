e3491c286c5e9a54931ec60515c965ff5ebd1fbf
Michael Kelley <mickelley@microsoft.com>
2024-09-17 12:55:06 -0700

    Should work with yarn 2 and greater

--- src/Helpers/Get-Packages.ps1
+++ src/Helpers/Get-Packages.ps1
@@ -6,13 +6,25 @@ param ( $CommandName,
 
 . (Join-Path $PSScriptRoot "Find-PackageNameWithoutVersion.ps1")
 
-if ([string]::IsNullOrEmpty($WordToComplete)) {
-    $json = & yarn list --json | ConvertFrom-Json
+$version = & yarn --version
+if (-not $version) {
+    throw "Yarn is not installed"
 }
-else {
-    $json = & yarn list --pattern $WordToComplete --json | ConvertFrom-Json
+if ($version.split('.')[0] -le 1) {
+    if ([string]::IsNullOrEmpty($WordToComplete)) {
+        $json = & yarn list --json | ConvertFrom-Json
+    }
+    else {
+        $json = & yarn list --pattern $WordToComplete --json | ConvertFrom-Json
+    }
+    $names = $json.data.trees | ForEach-Object { 
+        Find-PackageNameWithoutVersion $_.name
+    }
+    return $names 
+} else {
+    $json = & yarn info --name-only --json --recursive | ConvertFrom-Json
+    $names = $json | ForEach-Object { 
+        Find-PackageNameWithoutVersion $_.Trim('"')
+    } | Select-Object -Unique
+    return $names 
 }
-$names = $json.data.trees | ForEach-Object { 
-    Find-PackageNameWithoutVersion $_.name
-}
-return $names 
