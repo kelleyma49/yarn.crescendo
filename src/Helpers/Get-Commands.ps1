c5952027c38c94185ec7117f2db1bb42533e7e89
Michael Kelley <michael.a.kelley@gmail.com>
2024-09-17 13:00:34 -0700

    Should work with yarn 2 and greater (#11)
    Co-authored-by: Michael Kelley <mickelley@microsoft.com>

--- src/Helpers/Get-Commands.ps1
+++ src/Helpers/Get-Commands.ps1
@@ -1,2 +1,11 @@
-$commands = & yarn --json --non-interactive run | ConvertFrom-Json
-return $commands.data.items
+$version = & yarn --version
+if (-not $version) {
+    throw "Yarn is not installed"
+}
+if ($version.split('.')[0] -le 1) {
+    $commands = & yarn --json --non-interactive run | ConvertFrom-Json
+    return $commands.data.items
+} else { # Yarn berry and above
+    $commands = & yarn run --json | ConvertFrom-Json
+    return $commands.name
+}
